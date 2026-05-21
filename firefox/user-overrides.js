5bbc67185ef9f98c42f2cbbf79f5f53346d52cbb
SyoBoN <syobon@syobon.net>
2026-05-21 19:41:03 +0900

    firefox: initial

---
+++ firefox/user-overrides.js
@@ -0,0 +1,73 @@
+// 0102
+user_pref("browser.startup.page", 1);
+
+// 0103
+user_pref("browser.startup.homepage", "about:home");
+
+// 0104
+user_pref("browser.newtabpage.enabled", true);
+
+// 0401
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+
+// 0710
+user_pref("network.trr.mode", 3);
+
+// 0712
+//user_pref("network.trr.uri", "https://example.net");
+//user_pref("network.trr.custom_uri", "https://example.net");
+
+// 0807
+user_pref("browser.urlbar.clipboard.featureGate", false);
+
+// 0808
+user_pref("browser.urlbar.recentsearches.featureGate", false);
+
+// 0815
+user_pref("browser.urlbar.suggest.engines", false);
+
+// 0820
+user_pref("layout.css.visited_links_enabled", false);
+
+// 0830
+user_pref("browser.search.separatePrivateDefault", false);
+user_pref("browser.search.separatePrivateDefault.ui.enabled", false);
+
+// 1223
+user_pref("security.cert_pinning.enforcement_level", 1);
+
+// 2654
+user_pref("browser.download.always_ask_before_handling_new_types", false);
+
+// 4501
+user_pref("privacy.resistFingerprinting.pbmode", true);
+
+// 5003
+user_pref("signon.rememberSignons", false);
+
+// 5005
+user_pref("security.nocertdb", true);
+
+// 5017
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+
+// 5508
+user_pref("media.eme.enabled", false);
+user_pref("browser.eme.ui.enabled", false);
+
+// 5510
+user_pref("network.http.referer.XOriginPolicy", 2);
+
+// 5511
+user_pref("network.trr.bootstrapAddr", "1.1.1.1");
+
+// Non-Arkenfox
+
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.tabs.loadBookmarksInTabs", true);
+user_pref("layout.spellcheckDefault", 0);
+user_pref("sidebar.main.tools", "history,bookmarks");
+user_pref("sidebar.revamp", true);
+user_pref("sidebar.verticalTabs", true);
